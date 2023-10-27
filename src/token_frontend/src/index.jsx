7c7a994b2f8e3ec24877584746eae3bd3a7261bf
Chiksinafees <nafeesahmed01.na@gmail.com>
2023-10-27 16:27:58 +0530

    Authentication with internet identity

--- src/token_frontend/src/index.jsx
+++ src/token_frontend/src/index.jsx
@@ -1,14 +1,27 @@
-
-import { createRoot } from 'react-dom/client';
-import React from 'react';
+import { createRoot } from "react-dom/client";
+import React from "react";
 import App from "./components/App";
+import { AuthClient } from "@dfinity/auth-client";
 
-const init = async () => { 
-  const container = document.getElementById("root");
-  const root = createRoot(container);
-  
-  root.render(<App />);
-}
+const init = async () => {
+  const authClient = await AuthClient.create();
+
+  if (await authClient.isAuthenticated()) {
+    handleAuthenticate(authClient);
+  } else {
+    await authClient.login({
+      identityProvider: "https://identity.ic0.app/#authorize",
+      onSuccess: () => {
+        handleAuthenticate(authClient);
+      },
+    });
+  }
+
+  async function handleAuthenticate(authClient) {
+    const container = document.getElementById("root");
+    const root = createRoot(container);
+    root.render(<App />);
+  }
+};
 
 init();
-
